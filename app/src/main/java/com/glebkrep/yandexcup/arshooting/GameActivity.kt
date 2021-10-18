c6c9e1af48ad3706aed32986bb3b7fd98252fbad
Gleb Krepky <glebkrepp@gmail.com>
2021-10-18 14:43:08 +0300

    using google codelab code to show objects by gps coordinates

---
+++ app/src/main/java/com/glebkrep/yandexcup/arshooting/GameActivity.kt
@@ -0,0 +1,227 @@
+package com.glebkrep.yandexcup.arshooting
+
+import android.annotation.SuppressLint
+import android.app.ActivityManager
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import com.google.ar.sceneform.AnchorNode
+import com.glebkrep.yandexcup.arshooting.ar.PlaceNode
+import com.glebkrep.yandexcup.arshooting.ar.PlacesArFragment
+import com.glebkrep.yandexcup.arshooting.ar.model.Geometry
+import com.glebkrep.yandexcup.arshooting.ar.model.GeometryLocation
+import com.glebkrep.yandexcup.arshooting.ar.model.Place
+import com.glebkrep.yandexcup.arshooting.ar.model.getPositionVector
+
+class GameActivity : AppCompatActivity(), SensorEventListener {
+
+    private val TAG = "MainActivity"
+
+    private lateinit var arFragment: PlacesArFragment
+
+    // Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    // Sensor
+    private lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+
+    private var anchorNode: AnchorNode? = null
+    private var places: List<Place>? = null
+    private var currentLocation: Location? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!isSupportedDevice()) {
+            return
+        }
+        setContentView(R.layout.activity_game)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.ar_fragment) as PlacesArFragment
+
+        sensorManager = getSystemService()!!
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        setUpAr()
+        setUpMaps()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    private fun setUpAr() {
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            // Create anchor
+            val anchor = hitResult.createAnchor()
+            anchorNode = AnchorNode(anchor)
+            anchorNode?.setParent(arFragment.arSceneView.scene)
+            addPlaces(anchorNode!!)
+        }
+    }
+
+    private fun addPlaces(anchorNode: AnchorNode) {
+        val currentLocation = currentLocation
+        if (currentLocation == null) {
+            Log.w(TAG, "Location has not been determined yet")
+            return
+        }
+
+        val places = places
+        if (places == null) {
+            Log.w(TAG, "No places to put")
+            return
+        }
+
+        for (place in places) {
+            // Add the place in AR
+            val placeNode = PlaceNode(this, place)
+            placeNode.setParent(anchorNode)
+            placeNode.localPosition = place.getPositionVector(orientationAngles[0], currentLocation.latLng)
+            placeNode.setOnTapListener { _, _ ->
+                showInfoWindow(place)
+            }
+        }
+    }
+
+    private fun showInfoWindow(place: Place) {
+        // Show in AR
+        val matchingPlaceNode = anchorNode?.children?.filter {
+            it is PlaceNode
+        }?.first {
+            val otherPlace = (it as PlaceNode).place ?: return@first false
+            return@first otherPlace == place
+        } as? PlaceNode
+//        matchingPlaceNode?.showInfoWindow()
+    }
+
+
+    private fun setUpMaps() {
+        getCurrentLocation {
+            places = getNearbyPlaces()
+            for (place in places?: listOf()){
+                showInfoWindow(place)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation(onSuccess: (Location) -> Unit) {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            currentLocation = location
+            onSuccess(location)
+        }.addOnFailureListener {
+            Log.e(TAG, "Could not get location")
+        }
+    }
+
+    private fun getNearbyPlaces():List<Place> {
+        return listOf(
+            Place(
+                id = "0",
+                icon = "",
+                name = "Конец справа",
+                geometry = Geometry(GeometryLocation(
+                    59.94065, 30.384895
+                ))
+            ),
+            Place(
+                id = "1",
+                icon = "",
+                name = "Конец слева",
+                geometry = Geometry(GeometryLocation(
+                    59.939938,30.386433
+                ))
+            ),
+            Place(
+                id = "2",
+                icon = "",
+                name = "Через дорогу",
+                geometry = Geometry(GeometryLocation(
+                    59.940382,30.385033
+                ))
+            ),
+            Place(
+                id = "3",
+                icon = "",
+                name = "Сзади",
+                geometry = Geometry(GeometryLocation(
+                    59.940681, 30.385757
+                ))
+            )
+        )
+    }
+
+
+    private fun isSupportedDevice(): Boolean {
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val openGlVersionString = activityManager.deviceConfigurationInfo.glEsVersion
+        if (openGlVersionString.toDouble() < 3.0) {
+            Toast.makeText(this, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                .show()
+            finish()
+            return false
+        }
+        return true
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) {
+            return
+        }
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+    }
+}
+
+val Location.latLng: LatLng
+    get() = LatLng(this.latitude, this.longitude)
+
